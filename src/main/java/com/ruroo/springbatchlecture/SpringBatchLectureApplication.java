074965d8fc15cfa56272108d2274f6444df5676a
ddizi <ruroo@naver.com>
2023-11-11 16:54:27 +0900

    initial commit

---
+++ src/main/java/com/ruroo/springbatchlecture/SpringBatchLectureApplication.java
@@ -0,0 +1,13 @@
+package com.ruroo.springbatchlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchLectureApplication.class, args);
+    }
+
+}
